3892ca6dbafa03c709c156a6a6754f097291a71a
Raman Ratnakar <ramanratnakar09@gmail.com>
2020-05-21 18:33:52 +0530

    Final Commit(coding done)

---
+++ realtors/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    #list_editable = ('is_mvp',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+# Register your models here.
+admin.site.register(Realtor, RealtorAdmin)
